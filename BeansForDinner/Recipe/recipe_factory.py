abe2bd95bf069bc5c61bc8d140043a716831eea5
fsodari <franksodari@gmail.com>
2021-06-27 14:38:55 -0700

    recipe factory working better

--- BeansForDinner/Recipe/recipe_factory.py
+++ BeansForDinner/Recipe/recipe_factory.py
@@ -1,85 +1,118 @@
 import yaml
 from .recipe import Recipe
-import yaml
+import os
+
 # The recipe factory will create new recipe classes from human-readable yaml files, recipe templates
 # or from the user interface tools.
 
-# These are the generic base methods that will be used to generate new methods based on the recipe config.
-def rcpf_name(self:Recipe, name:str) -> str:
-    return name
+# This is where all of the config setttings are stored in each class.
+rcp_cfg_atr = 'rcp_config'
 
-def rcpf_cooking_time(self: Recipe, cooking_time:float) -> float:
-    return cooking_time
+def rcp_file_basename(filepath):
+    return os.path.splitext(os.path.basename(filepath))[0]
 
-# Populate/update methods if they exist in the config file.
-def get_main_methods(config:dict, methods:dict) -> dict:
-    if 'name' in config:
-        methods['name'] = lambda self: rcpf_name(self, config['name'])
-    if 'cooking_time' in config:
-        methods['cooking_time'] = lambda self: rcpf_cooking_time(self, config['cooking_time'])
-    return methods
-
-# Allow higher-level recipes to override lower level recipes.
-def override_main_methods(self:Recipe, config:dict):
-    if 'name' in config:
-        setattr(self, 'name', lambda self: rcpf_name(self, config['name']))
-    if 'cooking_time' in config:
-        setattr(self, 'cooking_time', lambda self: rcpf_cooking_time(self, config['cooking_time']))
+# Allow higher-level recipes to override lower level recipe attributes.
+def atomic_init(self:Recipe, config:dict={}):
+    print(config)
+    # Override attrs using ones provided in the config.
+    temp_cfg = getattr(self, rcp_cfg_atr)
+    for k in config:
+        temp_cfg[k] = config[k]
+    setattr(self, rcp_cfg_atr, temp_cfg)
 
 # Atomic recipe factory. Atomic recipes accept no ingredients as construction parameters.
 # TODO: Atomics may not contain ingredients, but their methods can be overriden.
-def AtomicRecipeFactory(config:dict):
-    methods = get_main_methods(config, {})
-    return type(config['name'], (Recipe,), methods)
+def AtomicRecipeFactory(config:dict={}):
+    class_config = {}
+    class_config['__init__'] = atomic_init
+    class_config[rcp_cfg_atr] = config
 
+    return type(config['name'], (Recipe,), class_config)
+
+# Params is a dict of recipe config dicts. If the param override matches, replace it.
+def composite_init(self:Recipe, config:dict={}):
+    temp_cfg = getattr(self, rcp_cfg_atr)
+    print(temp_cfg)
+    # Initialize default ingredients.
+    for k in temp_cfg['ingredients']:
+        # Override the configuration with a class
+        ingr_rcp = RecipeFactory(temp_cfg['ingredients'][k])
+        temp_cfg['ingredients'][k] = ingr_rcp
+
+    # Override ingredients.
+    if 'ingredients' in config:
+        for k in config['ingredients']:
+            ingr_rcp = RecipeFactory(config['ingredients'][k])
+            # Override the defaults
+            temp_cfg['ingredients'][k] = ingr_rcp
+    setattr(self, rcp_cfg_atr, temp_cfg)
 
 # Composites can contain multiple recipes as ingredients. The default ingredients
 # can be overriden with arguments in the constructor.
 def CompositeRecipeFactory(config:dict):
-    # Params is a dict of class definitions. If the param override matches, replace it.
-    def composite_init(self:Recipe, defaults:dict, params:dict={}):
-        # Override defaults
-        for p in params:
-            if p in defaults:
-                defaults[p] = params[p]
-        # Update the ingredients member to have instances of all classes.
-        self.ingredients = {k:defaults[k] for k in defaults}
-    
-    methods = get_main_methods(config, {})
-    # Add default classes.
-    defaults = {k:RecipeFactory(config['ingredients'][k]['source'])() for k in config['ingredients']}
-
+    class_config = {}
     # Init method takes optional parameters
-    methods['__init__'] = lambda self, params={}: composite_init(self, defaults, params)
+    class_config['__init__'] = composite_init
+    class_config[rcp_cfg_atr] = config
     # methods['ingredients'] = lambda self: 
-    return type(config['name'], (Recipe,), methods)
+    return type(config['name'], (Recipe,), class_config)
+
+def collection_new(cls:Recipe, variants:dict, config:dict={}):
+    # Enforce a default option in collections.
+    if 'which' not in config:
+        which = next(iter(variants))
+    else:
+        which = config['which']
+    
+    # Decide what to do based on what which is...wait what?
+    try:
+        recipe_choice = variants[which]
+    except TypeError as e:
+        if type(which) is dict:
+            recipe_choice = which
+        else:
+            raise TypeError('Collection overrides must use dicts')
+    except KeyError as e:
+        if type(which) is str:
+            recipe_choice = {'name':which}
+    # If source is not in the choice, create an atomic using the name.
+    if 'source' not in recipe_choice.keys() and 'name' not in recipe_choice.keys():
+        recipe_choice['name'] = which
+    
+    recipe_class = RecipeFactory(recipe_choice)
+    return recipe_class(recipe_choice)
 
 # Collections contain a dict of recipes and recipe overrides.
 # Choose which recipe you want to use at object creation time. 
 def CollectionFactory(config:dict):
-    def collection_new(cls:Recipe, variants:dict, which:str):
-        # Enforce a default option in collections.
-        if which is None:
-            which = next(iter(variants))
-        
-        recipe_choice = variants[which]
-        inst = RecipeFactory(recipe_choice['source'])
-        # Apply collection overrides
-        override_main_methods(inst, recipe_choice)
-        return inst()
-
     # Find source of the specific recipe we will implement
     variants = config['variants']
-    methods = get_main_methods(config, {})
-    methods['__new__'] = lambda cls, which=None: collection_new(cls, variants, which)
-    return type(config['name'], (Recipe,), methods)
+    class_config = {}
+    class_config[rcp_cfg_atr] = config
+    class_config['__new__'] = lambda cls, which={}: collection_new(cls, variants, which)
+    return type(config['name'], (Recipe,), class_config)
 
 # Generic recipe factory creator. This will parse the config file
 # and figure out which category it is based on the config file.
-def RecipeFactory(config_file:str):
-    # Read in yaml file.
-    with open(config_file, 'r') as stream:
-        config = yaml.safe_load(stream)
+def RecipeFactory(config:dict):
+    # If no source is supplied, an atomic will be used
+    if 'source' in config.keys():
+        config_file = config['source']
+        filebasename = rcp_file_basename(config_file)
+        # Read in yaml file.
+        try:
+            with open(config_file, 'r') as stream:
+                config = yaml.safe_load(stream)
+                if config == None:
+                    config = {'name':filebasename}
+        except FileNotFoundError:
+            # If the file doesn't exist, create a new atomic recipe using the file name.
+            if 'name' not in config.keys():
+                config = {'name':filebasename}
+
+    # A name is required for recipes created from a dict
+    if 'name' not in config.keys():
+        raise KeyError(f"Recipes must have a name!")
     # Collections implementation. Returns a lambda that accepts arguments.
     if 'variants' in config:
         return CollectionFactory(config)
--- tests/test_recipe_factory.py
+++ tests/test_recipe_factory.py
@@ -1,6 +1,70 @@
+from BeansForDinner.Recipe.recipe import Recipe
+import pytest
 from BeansForDinner import RecipeFactory
 
-def test_basic():
-    steel_class = RecipeFactory('recipe_config/SteelCutOats.yml')
+def test_atomic_defaults():
+    steel_class = RecipeFactory({'source':'recipe_config/SteelCutOats.yml'})
     steel_inst = steel_class()
-    assert steel_inst.name() == 'Steel Cut Oats'
+    print(steel_inst.rcp_config)
+    # This should follow default arguments
+    assert steel_inst.rcp_config['name'] == 'Steel Cut Oats'
+    assert steel_inst.rcp_config['cooking_time'] == 14.0
+
+def test_atomic_overrides():
+    steel_class = RecipeFactory({'source':'recipe_config/SteelCutOats.yml'})    
+    # Supply config overrides at construction.
+    steel_inst = steel_class({'name':'sco', 'cooking_time':3.14})
+    assert steel_inst.rcp_config['name'] == 'sco'
+    assert steel_inst.rcp_config['cooking_time'] == 3.14
+
+def test_atomic_empty():
+    # Recipes must be named!
+    with pytest.raises(KeyError):
+        what_class = RecipeFactory({})
+    
+    # Try reading a non-existant recipe from source.
+    foo_class = RecipeFactory({'source':'recipe_config/Foo.yml'})
+    foo_inst = foo_class()
+    assert foo_inst.rcp_config['name'] == 'Foo'
+
+    # Try reading a recipe from an empty file
+    water_class = RecipeFactory({'source':'recipe_config/Water.yml'})
+    water_inst = water_class()
+    assert water_inst.rcp_config['name'] == 'Water'
+
+def test_composite_basic():
+    oatmeal_class = RecipeFactory({'source':'recipe_config/Oatmeal.yml'})
+    oatmeal_inst = oatmeal_class()
+    assert oatmeal_inst.rcp_config['name'] == 'Oatmeal'
+
+    # You cannot reuse a class created from RecipeFactory. It must be a new instance.
+    oatmeal2 = RecipeFactory({'source':'recipe_config/Oatmeal.yml'})
+    oats2 = oatmeal2({'ingredients':{'base':{'source':'recipe_config/RolledOats.yml'}}})
+    assert oats2.rcp_config['name'] == 'Oatmeal'
+    assert oats2.rcp_config['ingredients']['base'].rcp_config['name'] == 'Rolled Oats'
+
+def test_collection_default():
+    oats_class = RecipeFactory({'source':'recipe_config/Oats.yml'})
+    oats_inst = oats_class()
+    assert oats_inst.rcp_config['name'] == 'Steel Cut Oats'
+    # Make sure it's using the class overrides
+    assert oats_inst.rcp_config['cooking_time'] == 13.0
+
+def test_collection_option():
+    # Select a different option.
+    oats_class = RecipeFactory({'source':'recipe_config/Oats.yml'})
+    oats = oats_class({'which':'Rolled Oats'})
+    assert oats.rcp_config['name'] == 'rollllled oats'
+    assert oats.rcp_config['cooking_time'] == 5.0
+
+def test_collection_new():
+    # If the choice is not in the collection, an empty atomic is created.
+    oats_class = RecipeFactory({'source':'recipe_config/Oats.yml'})
+    oats = oats_class({'which':'Groats'})
+    assert oats.rcp_config['name'] == 'Groats'
+
+    # Overriding collection with new option created on the fly.
+    oats_class = RecipeFactory({'source':'recipe_config/Oats.yml'})
+    oats = oats_class({'which':{'name':'Groats', 'cooking_time':20.0}})
+    assert oats.rcp_config['name'] == 'Groats'
+    assert oats.rcp_config['cooking_time'] == 20.0
